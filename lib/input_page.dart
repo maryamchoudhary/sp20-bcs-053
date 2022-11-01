60f865b045713b4ebe6eadec147e41e3e6000eef
maryamchoudhary <115917238+maryamchoudhary@users.noreply.github.com>
2022-10-31 22:32:59 -0700

    themelecture55
    themecode

---
+++ lib/input_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:bmicalculator/input_page.dart';
+
+class InputPage extends StatefulWidget {
+
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar(
+title: Text('BMI Calculator'),
+),
+body: Center(
+child: Text('Body Text'),
+),
+);
+  }
+}
+
+class InputPageState extends State<InputPage>{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+    );
+  }
+}
