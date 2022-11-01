d3d0da1202e95e12fb994a328e36cc4d7d0c3530
maryamchoudhary <115917238+maryamchoudhary@users.noreply.github.com>
2022-10-31 23:03:21 -0700

    lecture56
    create code design column and row

--- bmipractice/lib/input_page.dart
+++ bmipractice/lib/input_page.dart
@@ -15,8 +15,52 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget> [
+          Expanded(child: Row(
+            children: <Widget> [
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1d1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1d1E33),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),),
+            ],
+          ),),
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF1d1E33),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),),
+          Expanded(child: Row(
+          children: <Widget> [
+    Expanded(child: Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1d1E33),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),),
+    Expanded(child: Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1d1E33),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),),
+    ],
+    ),),
+        ],
       ),
     );
   }
