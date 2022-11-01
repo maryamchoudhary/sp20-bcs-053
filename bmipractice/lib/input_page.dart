48e477b198e0a641933753a025b2365379bf71e3
maryamchoudhary <115917238+maryamchoudhary@users.noreply.github.com>
2022-11-01 00:11:53 -0700

    lecture57
    create refractor icon code

--- bmipractice/lib/input_page.dart
+++ bmipractice/lib/input_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bmipractice/input_page.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class InputPage extends StatefulWidget {
 
 
@@ -21,10 +21,18 @@ class _InputPageState extends State<InputPage> {
             children: <Widget> [
               Expanded(child: RepeatContainerCode(
           colors: Color(0xFF1D1E33),
+      cardWidget: RepeatTextandICONWidget(
+        iconData: FontAwesomeIcons.male,
+        label: 'Male',
+      ),
       ),),
               Expanded(child: RepeatContainerCode(
     colors: Color(0xFF1D1E33),
-    ),),
+    cardWidget: RepeatTextandICONWidget(
+    iconData: FontAwesomeIcons.female,
+    label: 'Female',
+    ),
+               ),
           Expanded(child: RepeatContainerCode(
     colors: Color(0xFF1D1E33),
     ),),
@@ -46,13 +54,38 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class RepeatTextandICONWidget extends StatelessWidget {
+  RepeatTextandICONWidget({required this.iconData,required this.label});
+ final IconData iconData;
+ final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: <Widget>[
+          Icon(
+            iconData,
+            size: 80.0,
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Text('label',style: TextStyle(
+            fontSize: 30.0,
+          ),)
+        ]
+    );
+  }
+}
+
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
-  Color colors;
+  RepeatContainerCode({required this.colors,required this.cardWidget});
+  final Color colors;
+  final Widget cardWidget;
 @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardWidget,
       decoration: BoxDecoration(
         color: Color(0xFF1d1E33),
         borderRadius: BorderRadius.circular(10.0),
