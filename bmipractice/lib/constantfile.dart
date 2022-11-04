f197945fcf322f376f101a82ea7521aaf5db2402
maryamchoudhary <115917238+maryamchoudhary@users.noreply.github.com>
2022-11-04 08:40:40 -0700

    lecture 69
    complete ui 2nd screen in BMI

--- bmipractice/lib/constantfile.dart
+++ bmipractice/lib/constantfile.dart
@@ -16,3 +16,24 @@ const klargeButtonstyle = TextStyle(
   fontSize: 25.0,
   fontWeight: FontWeight.bold,
 );
+const kTitleStyleS2 = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const ResultTextstyle=TextStyle(
+  fontSize: 25,
+  color: Colors.pink,
+  fontWeight: FontWeight.bold,
+);
+const ResultStyle=TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.bold,
+);
+const BodyTextStyle=TextStyle(
+  fontSize: 20,
+  color: Colors.grey,
+);
+const Labelstyle2=TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.bold,
+);
--- bmipractice/lib/resultFile.dart
+++ bmipractice/lib/resultFile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'constantfile.dart';
+import 'containerfile.dart';
+import 'input_page.dart';
 class resultscreen extends StatelessWidget {
   const resultscreen({Key? key}) : super(key: key);
 
@@ -8,7 +11,52 @@ class resultscreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI Result'),
       ),
-      body: Text('body data'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+              child: Container(
+                  child: Center(
+                      child: Text('Your Result', style:kTitleStyleS2,)
+                  ),
+              ),
+          ),
+          Expanded(
+              flex: 5,
+              child: RepeatContainerCode(
+                  colors: activeColor,
+                  cardwidget: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(ResultText.toUpperCase(),style: ResultTextstyle,),
+    Text(bmiResult,style: ResultStyle,),
+    Text(interpretation,
+    textAlign: TextAlign.center,
+    style: BodyTextStyle,),
+                      ],
+                  ),
+              ),
+          ),
+          Expanded(child:  GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> InputPage()));
+            },
+              child: Container(
+                  child:
+                  Center(
+    child: Text('Re-Calculate',style: Labelstyle2,)),
+    color: Color(0xFFEB1555),
+    margin: EdgeInsets.only(top: 10),
+    width: double.infinity,
+    height: 50,
+                  ),
+              ),
+          ),
+          ),
+        ],
+      ),
     );
   }
 }
