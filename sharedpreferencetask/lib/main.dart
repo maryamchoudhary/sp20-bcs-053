4463624a78548231572ddbe4b30f65023385f432
maryamchoudhary <115917238+maryamchoudhary@users.noreply.github.com>
2022-12-05 23:54:22 -0800

    sharedpref

--- sharedpreferencetask/lib/main.dart
+++ sharedpreferencetask/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'count.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Shared preferences demo',
       home: MyHomePage(title: 'Shared preferences demo'),
+
     );
+
   }
 }
 
@@ -77,5 +79,55 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+class ListBuilder extends StatefulWidget {
+  const ListBuilder({
+    super.key,
+    required this.selectedList,
+    required this.isSelectionMode,
+    required this.onSelectionChange,
+  });
+
+  final bool isSelectionMode;
+  final List<bool> selectedList;
+  final Function(bool)? onSelectionChange;
+
+  @override
+  State<ListBuilder> createState() => _ListBuilderState();
+}
+
+class _ListBuilderState extends State<ListBuilder> {
+  void _toggle(int index) {
+    if (widget.isSelectionMode) {
+      setState(() {
+        widget.selectedList[index] = !widget.selectedList[index];
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.selectedList.length,
+        itemBuilder: (_, int index) {
+          return ListTile(
+              onTap: () => _toggle(index),
+              onLongPress: () {
+                if (!widget.isSelectionMode) {
+                  setState(() {
+                    widget.selectedList[index] = true;
+                  });
+                  widget.onSelectionChange!(true);
+                }
+              },
+              trailing: widget.isSelectionMode
+                  ? Checkbox(
+                value: widget.selectedList[index],
+                onChanged: (bool? x) => _toggle(index),
+              )
+                  : const SizedBox.shrink(),
+              title: Text('item $index'));
+        });
   }
 }
